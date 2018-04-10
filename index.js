87779a2bb45da31edb3e5f4181522e382cfcf50b
Phoebe Lau <phoebelau7@gmail.com>
2018-04-10 16:05:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-const app = "I don't do much.";
-fetch('https://api.github.com/repos/jquery/jquery/commits')
-  .then(res => res.json())
-  .then(json => console.log(json));
+const token = '152cfd5fbfd857ab32de948c97d28163524d0f7d'
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).then(res => res.json()).then(json => console.log(json));
